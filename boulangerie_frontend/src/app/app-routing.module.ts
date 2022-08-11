ba604ebc492bdfd1d4e12f56360dc3ffbd95f849
Grégoire Antoine <he201703@students.ephec.be>
2022-08-11 11:56:17 +0200

    Page Home #7
    Ajout de la page Home reprenant les informations générale de la boulangerie.

--- boulangerie_frontend/src/app/app-routing.module.ts
+++ boulangerie_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticleComponent } from './component/article/article.component';
+import { HomeComponent } from './component/home/home.component';
 import { ArticleCategoryComponent } from './component/article-category/article-category.component'
 import { DateorderComponent } from './component/dateorder/dateorder.component';
 const routes: Routes = [
-
+  {path:'home', component: HomeComponent},
   {path:'products', component: ArticleComponent},
   {path:'article_category', component: ArticleCategoryComponent},
   {path:'dateorder', component: DateorderComponent},
--- boulangerie_frontend/src/app/app.module.ts
+++ boulangerie_frontend/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './component/home/home.component';
 import { ArticleComponent } from './component/article/article.component';
 import { AppComponent } from './app.component';
 import { ArticleCategoryComponent } from './component/article-category/article-category.component';
 import { CartComponent } from './component/cart/cart.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DateorderComponent } from './component/dateorder/dateorder.component';
 import { FormsModule } from '@angular/forms';
 import { NgxHorizontalTimelineModule } from 'ngx-horizontal-timeline';
-import { DateorderComponent } from './component/dateorder/dateorder.component';
+
 @NgModule({
 
   declarations: [
     AppComponent,
     ArticleComponent,
+    HomeComponent,
     ArticleCategoryComponent,
     CartComponent,
     DateorderComponent
