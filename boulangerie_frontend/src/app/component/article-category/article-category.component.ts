d7d2058461adf20a1edb41c43976bc12aae73a7a
Grégoire Antoine <he201703@students.ephec.be>
2022-08-11 11:32:45 +0200

    Mise en place du tri par catégorie #4
    Ce commit ajoute au projet la page servant à trier et affiché par catégorie les produits sur le site.

--- boulangerie_frontend/src/app/component/article-category/article-category.component.ts
+++ boulangerie_frontend/src/app/component/article-category/article-category.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, subscribeOn } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-
+import { CartService } from 'src/app/service/cart.service';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  selector: 'app-article-category',
+  templateUrl: './article-category.component.html',
+  styleUrls: ['./article-category.component.scss']
 })
-
-
-export class ArticleComponent implements OnInit {
-
+export class ArticleCategoryComponent implements OnInit {
   product:any;
+  product2:any[]  = [];
   category:any;
   products: any[]  = [];
   compteur: any[]  = [];
@@ -21,6 +19,7 @@ export class ArticleComponent implements OnInit {
   selectedOption : number =1;
   api_link : string='http://127.0.0.1:8000/api';
   productdata : string=''
+  a:any;
   nmbr_article(){
     var nombre=0;
     while(nombre<51){
@@ -31,7 +30,7 @@ export class ArticleComponent implements OnInit {
   counter(i: number) {
     return new Array(i);
 }
-  constructor(private http:HttpClient ) {
+  constructor(private http:HttpClient , private cartService : CartService) {
 
   }
 
@@ -45,27 +44,42 @@ export class ArticleComponent implements OnInit {
         'Authorization': 'Basic ' + btoa('gregoire:antoine21')
       })
     };
-    this.http.get('http://127.0.0.1:8000/api/product',httpOptions) //http://www.mocky.io/v2/5ea172973100002d001eeada
+
+
+    this.http.get('http://127.0.0.1:8000/api/product',httpOptions)
     .subscribe(Response => {
       this.product=Response;
-      this.quantity_number= new Array(50)
+      this.product2=[]
+      for (let compteur in Response){
 
+        if (this.product[compteur].product_category_id == this.getData()){
+          this.product2.push(this.product[compteur])
+
+        }
+      }
+
+
+
+      this.quantity_number= new Array(50)
     });
 
-    this.http.get('http://127.0.0.1:8000/api/productcategory',httpOptions) //http://www.mocky.io/v2/5ea172973100002d001eeada
+    this.http.get('http://127.0.0.1:8000/api/productcategory',httpOptions)
     .subscribe(Response => {
       this.category=Response;
-
     });
 
   }
 
-
-  filter_product(category : any){
-    console.log(category)
-    sessionStorage.setItem('category', category);
+  addtocart(item: any ,quantity: number){
+    this.cartService.addtoCart(item,quantity);
+    this.selectedOption=1
   }
 
+  filter_product(category : any){
+
+    sessionStorage.setItem('category', category);
+    this.ngOnInit();
+  }
   getData(){
     return sessionStorage.getItem('category');
   }
