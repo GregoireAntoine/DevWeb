f58e181cdb0da80d1fd30ee032919164130c8424
Grégoire Antoine <he201703@students.ephec.be>
2022-08-13 14:03:33 +0200

    Mise en place de la section permettant de récupérer un message et son sujet sur la page Home #13

--- boulangerie_frontend/src/app/app.module.ts
+++ boulangerie_frontend/src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './component/home/home.component';
-import { ArticleComponent } from './component/article/article.component';
+
 import { AppComponent } from './app.component';
-import { ArticleCategoryComponent } from './component/article-category/article-category.component';
-import { CartComponent } from './component/cart/cart.component';
+
+import { HttpClientModule} from '@angular/common/http';
+import { ArticleComponent } from './component/article/article.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HomeComponent } from './component/home/home.component';
+import { CartComponent } from './component/cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DateorderComponent } from './component/dateorder/dateorder.component';
-import { FormsModule } from '@angular/forms';
-import { NgxHorizontalTimelineModule } from 'ngx-horizontal-timeline';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { CheckoutComponent } from './component/checkout/checkout.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxHorizontalTimelineModule } from 'ngx-horizontal-timeline';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { OrderConfirmComponent } from './component/order-confirm/order-confirm.component';
+import { ArticleCategoryComponent } from './component/article-category/article-category.component';
 import { MycommandComponent } from './component/mycommand/mycommand.component';
+
 
 @NgModule({
 
@@ -28,9 +45,21 @@ import { MycommandComponent } from './component/mycommand/mycommand.component';
    ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     NgbModule,
+    AppRoutingModule,
     FormsModule,
-    NgxHorizontalTimelineModule
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MdbDropdownModule,
+    NgxHorizontalTimelineModule,
+    FontAwesomeModule
   ],
 
   bootstrap: [AppComponent]
--- boulangerie_frontend/src/app/component/home/home.component.ts
+++ boulangerie_frontend/src/app/component/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, subscribeOn } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { CartService } from 'src/app/service/cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +13,42 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class HomeComponent implements OnInit {
+  messageform = this.formBuilder.group({
+    subject: '',
+    message: ''
+  });
 
-  constructor() {
+  product:any;
+  products: any[]  = [];
+  compteur: any[]  = [];
+  quantity_number: any[]  = [];
+  selectedOption : number =1;
+  api_link : string='http://127.0.0.1:8000/api';
+  productdata : string=''
+
+
+  constructor(private http:HttpClient , private cartService : CartService, private formBuilder: FormBuilder) {
 
   }
 
 
 
 
+
+  onSubmit(): void {
+    const httpOptions = {
+
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + btoa(localStorage.getItem("user")+':'+localStorage.getItem("psw"))
+      })
+    };
+    // Process checkout data here
+    console.log(JSON.stringify(this.messageform.value))
+    this.http.post('http://127.0.0.1:8000/api/message',JSON.stringify(this.messageform.value),httpOptions).subscribe()
+    //console.warn('Your order has been submitted', this.messageform.value.message);
+    this.messageform.reset();
+  }
 
 
 
@@ -25,8 +56,9 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-  }
 
+
+  }
 
 
 
