c22bfb8d3c6ee86280f0d3ff3d85b63d5685603c
Grégoire Antoine <he201703@students.ephec.be>
2022-08-14 16:45:02 +0200

    Mise en forme du fichier Serializer
    Ajout des serializers de connexion et enregistrement #16 #15

--- boulangerie/boulangerie/serializers.py
+++ boulangerie/boulangerie/serializers.py
@@ -1,6 +1,21 @@
-from .models import ProductCategory, Product, Order, User, OrderLine
+from pyexpat import model
+import pdb
+from requests import post
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from .models import ProductCategory, Product, Order, User, OrderLine
+from django.contrib.auth import get_user_model, authenticate
+
+
+class ProductCategorySerializer(ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ["id", "name", "available_on_website"]
+
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        fields = ["subject", "message"]
 
 
 class ProductSerializer(ModelSerializer):
@@ -16,10 +31,10 @@ class ProductSerializer(ModelSerializer):
         ]
 
 
-class ProductCategorySerializer(ModelSerializer):
+class DeletePostSerializer(ModelSerializer):
     class Meta:
-        model = ProductCategory
-        fields = ["id", "name", "available_on_website"]
+        model = Order
+        fields = "__all__"
 
 
 class OrderSerializer(ModelSerializer):
@@ -27,6 +42,20 @@ class OrderSerializer(ModelSerializer):
         model = Order
         fields = [
             "user_id",
+            "date_order",
+            "date_takeaway",
+            "total_quantity",
+            "total_price",
+        ]
+
+
+class OrderCommandSerializer(ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "user_id",
+            "ref",
             "date_order",
             "date_takeaway",
             "total_quantity",
@@ -53,21 +82,49 @@ class OrderLineSerializer(ModelSerializer):
         fields = ["order_id", "product_id", "quantity", "price"]
 
 
-class DeletePostSerializer(ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__"
+UserModel = get_user_model()
 
 
-class OrderCommandSerializer(ModelSerializer):
+class RegisterSerializer(ModelSerializer):
+    username = serializers.CharField(required=False, allow_blank=True)
+    email = serializers.EmailField(required=False, allow_blank=True)
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def authenticate(self, **kwargs):
+        return authenticate(self.context["request"], **kwargs)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        email = attrs.get("email")
+        password = attrs.get("password")
+        user = None
+        try:
+            username = UserModel.objects.get(email__iexact=email).get_username()
+        except UserModel.DoesNotExist:
+            pass
+
+        if username:
+            user = email
+
+            ###user = self.validate_username_email(username, "", password)
+        import pdb
+
+        pdb.set_trace()
+        attrs["user"] = user
+        return self.context["request"]["data"]
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=False, allow_blank=True)
+    password = serializers.CharField(style={"input_type": "password"})
+
+    ###user = self.validate_username_email(username, "", password)
+
+
+class AccountSerializer(serializers.Serializer):
     class Meta:
-        model = Order
-        fields = [
-            "id",
-            "user_id",
-            "ref",
-            "date_order",
-            "date_takeaway",
-            "total_quantity",
-            "total_price",
-        ]
+        model = OrderLine
+        fields = ["username", "email", "password"]
+
+
+###request._full_data
