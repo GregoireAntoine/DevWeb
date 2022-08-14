b924f2eaba665d6a5352f1c58172572369695099
Grégoire Antoine <he201703@students.ephec.be>
2022-08-14 16:43:45 +0200

    Mise en forme de la page de routing angular

--- boulangerie_frontend/src/app/app-routing.module.ts
+++ boulangerie_frontend/src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
 import { ArticleComponent } from './component/article/article.component';
-import { HomeComponent } from './component/home/home.component';
-import { ArticleCategoryComponent } from './component/article-category/article-category.component'
 import { DateorderComponent } from './component/dateorder/dateorder.component';
 import { CheckoutComponent } from './component/checkout/checkout.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 import { OrderConfirmComponent } from './component/order-confirm/order-confirm.component'
+import { ArticleCategoryComponent } from './component/article-category/article-category.component'
 import { MycommandComponent } from './component/mycommand/mycommand.component';
-
 const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
   {path:'home', component: HomeComponent},
   {path:'products', component: ArticleComponent},
-  {path:'article_category', component: ArticleCategoryComponent},
+  {path:'cart', component: CartComponent},
   {path:'dateorder', component: DateorderComponent},
   {path:'checkout', component: CheckoutComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
   {path:'order_confirm', component: OrderConfirmComponent},
+  {path:'article_category', component: ArticleCategoryComponent},
   {path:'mycommand', component: MycommandComponent},
 ];
 
