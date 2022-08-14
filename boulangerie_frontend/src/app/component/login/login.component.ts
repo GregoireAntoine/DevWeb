d4663469990862a44dfb1c14b74336c8443b4489
Grégoire Antoine <he201703@students.ephec.be>
2022-08-14 16:46:18 +0200

    Ajout du module de connexion #15

---
+++ boulangerie_frontend/src/app/component/login/login.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup ,ReactiveFormsModule} from '@angular/forms';
+import { first } from 'rxjs/operators';
+import {AuthService} from 'src/app/service/auth.service';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { faTrash} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  faRibbon = faTrash;
+  data:any;
+  user : any;
+  userlogin:any;
+  message:any;
+  public myform :any = FormGroup;
+
+  constructor(private authService : AuthService,private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.myform = new FormGroup({
+      username : new FormControl(''),
+      password : new FormControl('')
+    });
+    const httpOptions = {
+
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + btoa(localStorage.getItem("user")+':'+localStorage.getItem("psw")),
+
+      })
+
+    };
+    this.userlogin = localStorage.getItem("user")
+    this.http.get("http://127.0.0.1:8000/api/mycommand",httpOptions)
+    .subscribe(Response => {
+
+    this.data=Response;
+
+    });
+  }
+  get f(){
+    return this.myform.controls
+  }
+
+  onSubmit(){
+
+    this.authService.login(this.f['username'].value,this.f["password"].value)
+    this.user = localStorage.getItem("token")
+    }
+
+    deconnection(){
+      localStorage.setItem("user","")
+      window.location.href = "http://localhost:4200/home";
+    }
+    deletefunction(valeur:any){
+      const httpOptions = {
+
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'Basic ' + btoa('gregoire:antoine21'),
+
+        })
+
+      };
+
+
+
+      this.http.delete('http://127.0.0.1:8000/api/delete/'+valeur+"/",httpOptions).subscribe();
+      this.ngOnInit()
+    }
+}
+
