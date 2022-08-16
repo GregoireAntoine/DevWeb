f34a25697c5f9a49233e2a87d94e14258a663c17
Grégoire Antoine <he201703@students.ephec.be>
2022-08-16 19:33:50 +0200

    modif settings database

--- boulangerie/boulangerie/settings.py
+++ boulangerie/boulangerie/settings.py
@@ -90,8 +90,18 @@ WSGI_APPLICATION = "boulangerie.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "boulangerie",
+        "USER": "django",
+        "PASSWORD": "Gregoire21#21",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "doratiotto",
@@ -100,7 +110,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "5432",
     }
-}
+} """
 
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
