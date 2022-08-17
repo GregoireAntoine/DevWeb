09f1cbd647ff48a8b73a88a52c103cc6f15b122a
Grégoire Antoine <he201703@students.ephec.be>
2022-08-17 19:48:59 +0200

    admin

--- boulangerie/boulangerie/urls.py
+++ boulangerie/boulangerie/urls.py
@@ -43,6 +43,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path("", admin.site.urls),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/productcategory", ProductCategoryAPIView.as_view()),
