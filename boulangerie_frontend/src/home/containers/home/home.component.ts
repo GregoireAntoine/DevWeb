a2bf4c8d75452e598b643ecf3913fe1572f3c176
FrancoisT97 <56149458+FrancoisT97@users.noreply.github.com>
2022-08-20 18:04:05 +0200

    Fix Thumbnail for bestseller

--- boulangerie_frontend/src/home/containers/home/home.component.ts
+++ boulangerie_frontend/src/home/containers/home/home.component.ts
@@ -4,6 +4,7 @@
   import { FormBuilder } from '@angular/forms';
   import { AuthService } from 'src/shared/services/auth.service';
   import { ItemsService } from 'src/shared/services/items.service';
+  import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
+
   messageform = this.formBuilder.group({
     subject: '',
     message: ''
@@ -26,7 +27,7 @@ export class HomeComponent implements OnInit {
   productdata : string=''
   endroitdata:number=0;
   issou:number=0;
-  
+
   numeroproduit:any;
   idleplusgrand:any;
   nmbr_article(){
@@ -39,34 +40,34 @@ export class HomeComponent implements OnInit {
   counter(i: number) {
     return new Array(i);
   }
-  
+
   constructor(private http:HttpClient , private cartService : CartService, private itemsService: ItemsService, private formBuilder: FormBuilder, private authService: AuthService) {
   }
-  
+
   ngOnInit(): void {
     this.user=localStorage.getItem("user")
- 
+
     this.itemsService.getBestSellers().subscribe(Response => {
         this.product=Response;
         this.quantity_number= new Array(50)
-        
+
         this.idleplusgrand=0
         for(this.numeroproduit in this.product ){
           if (this.idleplusgrand<this.product[this.numeroproduit]['id']){
             this.idleplusgrand=this.product[this.numeroproduit]['id']
           }
-          
+
         }
-        
+
         // Initialsiation d'un tableau de la taille valant l'id le plus grand plus 1 qui sera utilisé lors des select option dans la page article html
         while (this.endroitdata<this.idleplusgrand+1){
           this.selectedOption[this.endroitdata]=1
           this.endroitdata+=1
         }
-        
+
       });
   }
-  
+
   onSubmit(): void {
     // Process checkout data here
     console.log(JSON.stringify(this.messageform.value))
@@ -77,14 +78,24 @@ export class HomeComponent implements OnInit {
         }
         // get the status as error.status
       })
-    
+
     this.messageform.reset();
   }
-  
+
   addtocart(item: any ,quantity: number, product_id:any){
-    
+
     this.cartService.addtoCart(item,quantity);
     this.selectedOption[product_id]=1
+  }
+
+  getURLImage(urlImage:string): string{
+    if(urlImage){
+      return environment.serverUrl+urlImage
+    }
+    else{
+      return "assets/bread.png"
+    }
+
   }
 
 }
