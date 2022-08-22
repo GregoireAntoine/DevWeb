1446de18617d6b79303494573f026a9fe8f17f62
Grégoire Antoine <he201703@students.ephec.be>
2022-08-22 10:57:11 +0200

    ajout des commentaires

--- boulangerie_frontend/src/app/component/home/home.component.ts
+++ boulangerie_frontend/src/app/component/home/home.component.ts
@@ -3,6 +3,7 @@ import { Observable, subscribeOn } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { CartService } from 'src/app/service/cart.service';
 import { FormBuilder } from '@angular/forms';
+import { loadTranslations } from '@angular/localize';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,12 @@ import { FormBuilder } from '@angular/forms';
 
 
 export class HomeComponent implements OnInit {
+  // définition du formulaire et de ces clés
   messageform = this.formBuilder.group({
     subject: '',
     message: ''
   });
+  // Définitions des variables
   user:any;
   product:any;
   products: any[]  = [];
@@ -27,9 +30,9 @@ export class HomeComponent implements OnInit {
   productdata : string=''
   endroitdata:number=0;
   issou:number=0;
-
   numeroproduit:any;
   idleplusgrand:any;
+  // Fonction permettant d'introduire les nombres pour le choix de wuantité dans des option dres selects
   nmbr_article(){
     var nombre=0;
     while(nombre<51){
@@ -47,34 +50,33 @@ export class HomeComponent implements OnInit {
 
 
 
-
+  // Lorsque l'utilisateur clique sur envoyé
   onSubmit(): void {
     const httpOptions = {
-
+      // Définiton des paramètres de connexion à l'api
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
         'Authorization': 'Basic Z3JlZ29pcmU6YW50b2luZTIx'
       })
     };
-    // Process checkout data here
-    console.log(JSON.stringify(this.messageform.value))
+    // Envoie des données du formulaire au format json string
     this.http.post('https://admin.boulangerie.domaineprojetadmin.ovh/api/message',JSON.stringify(this.messageform.value),httpOptions).subscribe(
       data => {
-
+        //si le message est envoyé alors une alert prévient l'utilisateur
         if (data){
           alert("message envoyé")
         }
 
-           // get the status as error.status
-        })
 
+        })
+// Remise à 0 des champs du formulaire d'envoie de message
     this.messageform.reset();
   }
 
 
 
 
-
+// Lors dud démarrage de la page
   ngOnInit(): void {
     this.user=localStorage.getItem("user")
     const httpOptions = {
@@ -84,11 +86,12 @@ export class HomeComponent implements OnInit {
         'Authorization': 'Basic ' + btoa('gregoire:antoine21')
       })
     };
+    // Récupération des 3 produits les plus vendus.
     this.http.get('https://admin.boulangerie.domaineprojetadmin.ovh/api/bestsellers',httpOptions)
     .subscribe(Response => {
       this.product=Response;
       this.quantity_number= new Array(50)
-
+      // Création d'un tableau permettant de séléctionner les quantité indépendemment à chaque produits
       this.idleplusgrand=0
       for(this.numeroproduit in this.product ){
         if (this.idleplusgrand<this.product[this.numeroproduit]['id']){
@@ -106,7 +109,7 @@ export class HomeComponent implements OnInit {
     });
 
   }
-
+ // fonction d'ajout du produit dans le panier
   addtocart(item: any ,quantity: number, product_id:any){
 
     this.cartService.addtoCart(item,quantity);
