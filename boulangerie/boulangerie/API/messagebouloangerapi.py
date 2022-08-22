e3ab0efabbf2a1f15f55585e154d320e8f376f20
Grégoire Antoine <he201703@students.ephec.be>
2022-08-22 08:54:40 +0200

    ajout commentaire

--- boulangerie/boulangerie/API/messagebouloangerapi.py
+++ boulangerie/boulangerie/API/messagebouloangerapi.py
@@ -6,7 +6,9 @@ from rest_framework import status
 
 class MessageAPIView(APIView):
     # Envoie du message 
+    # on récupère le sujet et le message écrit par l'utilisateur
     def post(self, request, *args, **kwargs):
+        # on appel la fonction sent message
         sent_message(request.data["subject"], request.data["message"])
         return Response(request.data,status=status.HTTP_200_OK)
 
@@ -14,10 +16,12 @@ class MessageAPIView(APIView):
 def sent_message(subject, message):
         # fonction d'envoie du mail
         send_mail(
+            # on insère le sujet reçu en paramètre dans subject
+             # on insère le messsage reçu en paramètre dans message
             subject=subject,
             message=message,
-            from_email="no-reply@doratiotto.com",
-            recipient_list=["doratiottoboulangerie@gmail.com"],
+            from_email="no-reply@doratiotto.com", # email de départ
+            recipient_list=["doratiottoboulangerie@gmail.com"], # email de destination
             fail_silently=True,
         )
 
